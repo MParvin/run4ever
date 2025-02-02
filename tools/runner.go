3c7d0a3b2fcddcde7aad9d1f05ed941d6f9f6acf
Mohammad Parvin <m.parvin986@gmail.com>
2025-02-02 17:50:16 +0330

    add should notify function

--- tools/runner.go
+++ tools/runner.go
@@ -7,25 +7,62 @@ import (
 	"time"
 )
 
-func RunInfinitely(delayInt int, args []string, verbose bool) {
+func RunInfinitely(delayInt int, args []string, verbose bool , notifyOn string, notifyMethod string, telegramToken string, telegramChatID string, telegramCustomAPI string) {
 	for {
+		exitStatus := 0
+
 		cmd := exec.Command(args[0], args[1:]...)
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
 		cmd.Stdin = os.Stdin
 		err := cmd.Run()
+
 		if err != nil {
 			if verbose {
 				fmt.Println(err)
 			}
-			time.Sleep(time.Duration(delayInt) * time.Second)
-			continue
+			exitStatus = cmd.ProcessState.ExitCode() 
+		}
+
+		if ShouldNotify(notifyOn, exitStatus) {
+			title := "run4ever: Task " + StatusToString(exitStatus)
+			message := fmt.Sprintf("Command %s exited with status %d", args[0], exitStatus)
+			
+			switch notifyMethod {
+			case "desktop":
+				if err := SendDesktopNotification(title, message, verbose); err != nil && verbose {
+					fmt.Println("Error sending desktop notification: ", err)
+				}
+			case "telegram":
+				if err := SendTelegramNotification(telegramToken, telegramChatID, message, telegramCustomAPI ,verbose); err != nil && verbose {
+					fmt.Println("Error sending Telegram notification: ", err)
+				}
+			}
 		}
 		if verbose {
-			fmt.Printf("Command %s exited", args[0])
-			fmt.Print("Sleeping for ", delayInt, " seconds")
+			fmt.Printf("Command `%s` exited with status %d\n", args[0], exitStatus)
+			fmt.Printf("Sleeping for %d seconds\n", delayInt)
 		}
 		time.Sleep(time.Duration(delayInt) * time.Second)
 	}
+}
 
+func ShouldNotify(notifyOn string, exitStatus int) bool {
+	switch notifyOn {
+	case "always":
+		return true
+	case "success":
+		return exitStatus == 0
+	case "failure":
+		return exitStatus != 0
+	default:
+		return false
+	}
+}
+
+func StatusToString(exitStatus int) string {
+	if exitStatus == 0 {
+		return "Success"
+	}
+	return "Failure"
 }
